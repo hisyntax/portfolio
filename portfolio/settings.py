c568a4c90b33e7f0fe93bcc3ab0817359afcbdd1
purple-git <mathiasjiya2@gmail.com>
2020-12-07 17:28:57 -0800

    the first commit

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -72,10 +72,6 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
-        #'NAME': 'purple',
-        #'USER': 'postgres',
-        #'PASSWORD': 'giftOF463',
-        #'HOST': 'localhost'
 
     }
 }
